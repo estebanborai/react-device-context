efc9b789ecd3149880891c75b209aff56b08b319
Esteban Borai <estebanborai@gmail.com>
2020-05-17 16:56:57 -0300

    Release 0.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import DeviceContext from './context/device';
 
+export {
+  useCurrentBrowser, useCurrentFormat, useDeviceContext, useIsMobile,
+} from './hooks';
+
 export { DeviceContextProvider } from './context/device';
 export { DeviceFormat, DeviceProfile, Browser } from './service/device';
-export { useCurrentBrowser, useCurrentFormat, useDeviceContext } from './hooks';
 
 export default DeviceContext;
